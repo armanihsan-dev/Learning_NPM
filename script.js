313d5d7dc1b54be20b4e3232658314cff6b3ed64
arman ihsan <armanihsan224@gmail.com>
2025-02-05 16:06:32 +0500

    more last

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ import Quote from 'inspirational-quotes';
 // Make a request for a user with a given ID
 axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
   // handle success
-  console.log(response);
+  //console.log(response);
 });
+
+console.log(Quote.getQuote());

1e48e83c95d4c3726c0a5c25b6ca804fe240703b
arman ihsan <armanihsan224@gmail.com>
2025-02-05 15:56:42 +0500

    added inpirationl qt

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import Quote from 'inspirational-quotes';
 // Make a request for a user with a given ID
 axios.get('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
   // handle success
